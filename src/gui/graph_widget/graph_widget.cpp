Heavens132/hal

commit ea96e0b31e9324e01b4d48b34b8d53578728e480

first version of multi-driven nets

src/gui/graph_widget/graph_widget.cpp
@@ -307,7 +307,7 @@ void graph_widget::handle_navigation_left_request()
                 if (!n)
                     return;
 
-                if (n->get_src().gate == nullptr)
+                if (n->get_src().get_gate() == nullptr)
                 {
                     g_selection_relay.clear();
                     g_selection_relay.m_selected_nets.insert(n->get_id());
@@ -337,7 +337,7 @@ void graph_widget::handle_navigation_left_request()
             if (!n)
                 return;
 
-            if (n->get_src().gate != nullptr)
+            if (n->get_src().get_gate() != nullptr)
             {
                 handle_navigation_jump_requested(hal::node{hal::node_type::gate, 0}, n->get_id(), {n->get_src().get_gate()->get_id()});
             }
@@ -498,7 +498,7 @@ void graph_widget::handle_enter_module_requested(const u32 id)
         // is bad because that context won't react to any updates since empty
         // contexts can't infer their corresponding module from their contents
     }
-    
+
     if (m_context->gates().isEmpty() && m_context->modules() == QSet<u32>({id}))
     {
         m_context->unfold_module(id);
@@ -549,12 +549,12 @@ void graph_widget::ensure_gates_visible(const QSet<u32> gates)
         max_y = std::max(max_y, (int)rect.bottom());
     }
     auto targetRect = QRectF(min_x, min_y, max_x-min_x, max_y-min_y).marginsAdded(QMarginsF(20,20,20,20));
-    
+
     // FIXME This breaks as soon as the layouter call that preceded the call to this function
     // changed the scene size. If that happens, mapToScene thinks that the view is looking at (0,0)
     // and the animation jumps to (0,0) before moving to the correct target.
     auto currentRect = m_view->mapToScene(m_view->viewport()->geometry()).boundingRect(); // this has incorrect coordinates
-    
+
     auto centerFix = targetRect.center();
     targetRect.setWidth(std::max(targetRect.width(), currentRect.width()));
     targetRect.setHeight(std::max(targetRect.height(), currentRect.height()));

include/core/utils.h
@@ -191,6 +191,7 @@ namespace core_utils
             {
                 ss << joiner;
             }
+            first = false;
             ss << *it;
         }
         return ss.str();

include/gui/graph_widget/items/graphics_module.h
@@ -22,7 +22,7 @@ protected:
 
         bool operator==(const module_pin& rhs) const
         {
-            return this->net_id == rhs.net_id && this->pin_type == rhs.pin_type;
+            return this->net_id == rhs.net_id && this->pin_type == rhs.get_pin();
         }
     };
 

include/netlist/endpoint.h
@@ -33,24 +33,19 @@ class gate;
  *
  * @ingroup netlist
  */
-struct endpoint
+class endpoint
 {
-    std::shared_ptr<::gate> gate;
-
-    std::string pin_type;
-
-    /**
-    * Copies the value of an endpoint over to another endpoint.
-    *
-    * @param[in] copy - The endpoint to copy.
-    * @returns The target endpoint.
-    */
-    endpoint& operator=(const endpoint& copy)
+public:
+    endpoint(const std::shared_ptr<gate>& gate, const std::string& pin, bool is_a_dst)
     {
-        this->gate     = copy.gate;
-        this->pin_type = copy.pin_type;
-        return *this;
+        m_gate     = gate;
+        m_pin      = pin;
+        m_is_a_dst = is_a_dst;
     }
+    endpoint(const endpoint&) = default;
+    endpoint(endpoint&&)      = default;
+    endpoint& operator=(const endpoint&) = default;
+    endpoint& operator=(endpoint&&) = default;
 
     /**
     * Standard "less than". <br>
@@ -61,7 +56,15 @@ struct endpoint
     */
     bool operator<(const endpoint& rhs) const
     {
-        return (this->gate < rhs.gate) || ((this->gate == rhs.gate) && (this->pin_type < rhs.pin_type));
+        if (this->m_gate != rhs.m_gate)
+        {
+            return (this->m_gate < rhs.m_gate);
+        }
+        if (this->m_pin != rhs.m_pin)
+        {
+            return (this->m_pin < rhs.m_pin);
+        }
+        return (this->m_is_a_dst < rhs.m_is_a_dst);
     }
 
     /**
@@ -73,7 +76,7 @@ struct endpoint
     */
     bool operator==(const endpoint& rhs) const
     {
-        return (this->gate == rhs.gate) && (this->pin_type == rhs.pin_type);
+        return (this->m_gate == rhs.m_gate) && (this->m_pin == rhs.m_pin) && (this->m_is_a_dst == rhs.m_is_a_dst);
     }
 
     /**
@@ -94,35 +97,41 @@ struct endpoint
     */
     const std::shared_ptr<::gate>& get_gate() const
     {
-        return gate;
+        return m_gate;
     }
 
     /**
-     * Sets the gate of the endpoint/
-     * @param[in] g gate to be set
-     */
-    void set_gate(const std::shared_ptr<::gate>& g)
-    {
-        endpoint::gate = g;
-    }
-
-    /**
-     * Returns the pin type of the current endpoint
+     * Returns the pin of the current endpoint
      *
-     * @returns pin_type as std::string
+     * @returns pin as std::string
      */
-    const std::string& get_pin_type() const
+    const std::string& get_pin() const
     {
-        return pin_type;
+        return m_pin;
     }
 
     /**
-     * Sets the pin type of the current endpoint
+     * Checks the pin type of the current endpoint.
      *
-     * @param[in] type to be set
+     * @returns True, if the endpoint is an input pin.
      */
-    void set_pin_type(const std::string& type)
+    bool is_dst_pin() const
     {
-        endpoint::pin_type = type;
+        return m_is_a_dst;
     }
+
+    /**
+     * Checks the pin type of the current endpoint.
+     *
+     * @returns True, if the endpoint is an output pin.
+     */
+    bool is_src_pin() const
+    {
+        return !m_is_a_dst;
+    }
+
+private:
+    std::shared_ptr<gate> m_gate;
+    std::string m_pin;
+    bool m_is_a_dst;
 };

include/netlist/event_system/net_event_handler.h
@@ -47,7 +47,8 @@ namespace net_event_handler
         created,         ///< no associated_data
         removed,         ///< no associated_data
         name_changed,    ///< no associated_data
-        src_changed,     ///< no associated_data
+        src_added,       ///< associated_data = id of src gate
+        src_removed,     ///< associated_data = id of src gate
         dst_added,       ///< associated_data = id of dst gate
         dst_removed      ///< associated_data = id of dst gate
     };

include/netlist/gate.h
@@ -41,7 +41,7 @@
 class netlist;
 class net;
 class module;
-struct endpoint;
+class endpoint;
 
 /**
  * Gate class containing information about a gate including its location, functions, and module.

include/netlist/net.h
@@ -84,34 +84,81 @@ public:
      */
 
     /**
-     * Set the source of this net to a gate's output pin.
+     * Add a source to this net.
      *
      * @param[in] gate - The source gate.
-     * @param[in] pin_type - The pin of the source gate.
+     * @param[in] pin - The input pin of the gate.
      * @returns True on success.
      **/
-    bool set_src(const std::shared_ptr<gate>& gate, const std::string& pin_type);
+    bool add_src(const std::shared_ptr<gate>& gate, const std::string& pin);
 
     /**
-     * Set the source of this net to a gate's output pin.
+     * Add a source to this net by endpoint.
+     * If the endpoint is a destination-endpoint this function aborts.
      *
-     * @param[in] src - The source endpoint.
+     * @param[in] ep - The endpoint.
      * @returns True on success.
      **/
-    bool set_src(const endpoint& src);
+    bool add_src(const endpoint& ep);
 
     /**
-     * Remove the source of the net.
+     * Remove a source from this net.
      *
+     * @param[in] gate - The source gate.
+     * @param[in] pin - The input pin of the gate.
      * @returns True on success.
+     **/
+    bool remove_src(const std::shared_ptr<gate>& gate, const std::string& pin);
+
+    /**
+     * Remove a source from this net by endpoint.
+     * If the endpoint is a destination-endpoint this function aborts.
+     *
+     * @param[in] ep - The endoint.
+     * @returns True on success.
+     **/
+    bool remove_src(const endpoint& ep);
+
+    /**
+     * Check whether a gate is a source of this net.
+     *
+     * @param[in] gate - The source gate.
+     * @param[in] pin - The source output pin.
+     * @returns True if the gate's pin is a source of this net.
+     **/
+    bool is_a_src(const std::shared_ptr<gate>& gate, const std::string& pin) const;
+
+    /**
+     * Check whether an endpoint is a source of this net.
+     * If the endpoint is a destination-endpoint this function aborts.
+     *
+     * @param[in] ep - The endpoint.
+     * @returns True if the endpoint is a source of this net.
+     **/
+    bool is_a_src(const endpoint& ep) const;
+
+    /**
+     * Get the number of sources.<br>
+     * Faster than get_srcs().size().
+     *
+     * @returns The number of sources of this net.
      */
-    bool remove_src();
+    u32 get_num_of_srcs() const;
 
     /**
-     * Get the src of the net.
-     * If there was no src assigned, the gate element of the returned endpoint is a nullptr.
+     * Get the vector of sources of the net.
      *
-     * @returns The source endpoint.
+     * @param[in] filter - a filter for endpoints.
+     * @returns A vector of source endpoints.
+     */
+    std::vector<endpoint> get_srcs(const std::function<bool(const endpoint& ep)>& filter = nullptr) const;
+
+    /**
+     * Get the (first) src of the net.
+     * If there was no src assigned, the gate element of the returned endpoint is a nullptr.
+     * If the net is multi-driven a warning is printed.
+     *
+     * @returns The (first) source endpoint.
      */
     endpoint get_src() const;
 
@@ -123,49 +170,53 @@ public:
      * Add a destination to this net.
      *
      * @param[in] gate - The destination gate.
-     * @param[in] pin_type - The input pin of the gate.
+     * @param[in] pin - The input pin of the gate.
      * @returns True on success.
      **/
-    bool add_dst(const std::shared_ptr<gate>& gate, const std::string& pin_type);
+    bool add_dst(const std::shared_ptr<gate>& gate, const std::string& pin);
 
     /**
-     * Add a destination to this net.
+     * Add a destination to this net by endpoint.
      *
-     * @param[in] dst - The destination endpoint.
+     * @param[in] ep - The endpoint.
      * @returns True on success.
      **/
-    bool add_dst(const endpoint& dst);
+    bool add_dst(const endpoint& ep);
 
     /**
      * Remove a destination from this net.
+     * If the endpoint is a source-endpoint this function aborts.
      *
      * @param[in] gate - The destination gate.
-     * @param[in] pin_type - The input pin of the gate.
+     * @param[in] pin - The input pin of the gate.
      * @returns True on success.
      **/
-    bool remove_dst(const std::shared_ptr<gate>& gate, const std::string& pin_type);
+    bool remove_dst(const std::shared_ptr<gate>& gate, const std::string& pin);
 
     /**
-     * Remove a destination from this net.
+     * Remove a destination from this net by endpoint.
+     * If the endpoint is a source-endpoint this function aborts.
      *
-     * @param[in] dst - The destination endpoint.
+     * @param[in] ep - The endoint.
      * @returns True on success.
      **/
-    bool remove_dst(const endpoint& dst);
+    bool remove_dst(const endpoint& ep);
 
     /**
      * Check whether a gate is a destination of this net.
      *
      * @param[in] gate - The destination gate.
-     * @returns True if the input's pin is a destination.
+     * @param[in] pin - The destination output pin.
+     * @returns True if the gate's pin is a destination of this net.
      **/
-    bool is_a_dst(const std::shared_ptr<gate>& gate) const;
+    bool is_a_dst(const std::shared_ptr<gate>& gate, const std::string& pin) const;
 
     /**
-     * Check whether a gate's input pin is a destination of this net.
+     * Check whether an endpoint is a destination of this net.
+     * If the endpoint is a source-endpoint this function aborts.
      *
-     * @param[in] ep - The input endpoint.
-     * @returns True if the input's pin is a destination.
+     * @param[in] ep - The endpoint.
+     * @returns True if the endpoint is a destination of this net.
      **/
     bool is_a_dst(const endpoint& ep) const;
 
@@ -256,9 +307,7 @@ private:
     /** stores the name of the net */
     std::string m_name;
 
-    /** stores the src gate and pin id of src gate*/
-    endpoint m_src;
-
     /** stores the dst gate and pin id of the dst gate*/
     std::vector<endpoint> m_dsts;
+    std::vector<endpoint> m_srcs;
 };

include/netlist/netlist.h
@@ -40,7 +40,7 @@ class netlist_internal_manager;
 class net;
 class gate;
 class module;
-struct endpoint;
+class endpoint;
 
 /**
  * Netlist class containing information about the netlist including its gates, modules, and nets, as well as the underlying gate library.

commit 0c1a99f5be50c1ef1e608cedc0a83b13423de36b

Fix reacting to net API breakage from ea96e0b

src/gui/graph_widget/graph_widget.cpp
@@ -210,7 +210,8 @@ void graph_widget::handle_navigation_jump_requested(const hal::node origin, cons
         // If we don't have all the gates in the current context, we need to
         // insert them
 
-        bool netIsInput = n->is_a_destination(*gates.constBegin()); // either they're all inputs or all outputs, so just check the first one
+        auto in_nets = gates.constBegin()->get()->get_fan_in_nets(); // either they're all inputs or all outputs, so just check the first one
+        bool netIsInput = in_nets.find(n) != in_nets.cend();
         hal::placement_mode placement = netIsInput ? hal::placement_mode::prefer_right : hal::placement_mode::prefer_left;
         m_context->add({}, to_gates, hal::placement_hint{placement, origin});
 